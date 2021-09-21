690171ef9c17df6883dfd21cd5d050abd45c578f
Leonardo Soares <leonardoxp9d@gmail.com>
2021-09-21 11:53:19 -0300

    Initial commit

---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+/** isso vai fazer copiar os assets para o app nativo, 
+ * então esses assets da pasta raiz do nosso projeto vão precisar ficar dentro
+ * do app nativo
+*/
+module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: [
+      './assets/fonts/'
+    ],
+};
